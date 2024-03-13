660cfe87a9f9d847581fa3cf6c5cb10f051f6136
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2024-03-13 22:46:44 +0800

    added show notes

--- frontend/src/services/note.service.ts
+++ frontend/src/services/note.service.ts
@@ -15,6 +15,25 @@ export class NoteService {
     this.userService = userService;
   }
 
+  showNotes(): Observable<Note[]> {
+    // get jwt token from local stoarge
+    const token = localStorage.getItem('access_token');
+
+    // set the Authorization bearer token as the Jwt
+    const headers = {
+      Authorization: 'Bearer ' + token
+    };
+
+    // if token is not null
+    if (token) {
+      // decode the token
+      // set the username to the decoded token's username
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      const username = decodedToken.sub.username;
+    }
+    return this.http.get<Note[]>(this.apiUrl + '/notes', {headers})
+  }
+
   addNote(note: Note): Observable<any> {
     // get the jwt from local storage
     const token = localStorage.getItem('access_token');
--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
 import {User} from "../models/user";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import {User} from "../models/user";
 export class UserService {
   private apiUrl = 'http://localhost:5000/';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   loginUser(username: string, password: string): Observable<any> {
@@ -21,9 +22,26 @@ export class UserService {
     // return this.http.post(this.apiUrl + 'logout', {}, {headers: {Authorization: 'Bearer ' + localStorage.getItem('access_token')}});
     // get token from local storage
     const token = localStorage.getItem('access_token');
-    console.log(token)
-    return this.http.post(this.apiUrl + 'logout', {}, {headers: {Authorization: 'Bearer ' + token}});
+    // if response is status is 401, this means the token is invalid. log the user out
+    const headers = {
+      Authorization: 'Bearer ' + token
+    };
+
+    return this.http.post(this.apiUrl + 'logout', {}, {headers, observe: 'response'})
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            // Handle logout or token invalidation here
+            console.log('Token is invalid');
+            localStorage.removeItem('access_token');
+            // Redirect the user to the login page
+            this.router.navigate(['/login']);
+          }
+          return throwError(error);
+        })
+      );
   }
+
 
   registerUser(user: User): Observable<any> {
     return this.http.post(this.apiUrl + 'user', user);
